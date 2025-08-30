3f83e5c830a7cbbe523758671162b2ecf7e82eeb
sufiyankureshi688 <sufiyanmohammed688@gmail.com>
2025-08-30 15:26:49 +0530

    Update build_haiku.sh

--- scripts/build_haiku.sh
+++ scripts/build_haiku.sh
@@ -1,24 +1,21 @@
 #!/bin/bash
 set -e
 
-# File ID from Google Drive link
-FILE_ID="1jN423vDbJzPb0G9rmgGDi_HhbE73r_iI"
-DEST="haiku.zip"
+# Install gdown
+python3 -m pip install --quiet gdown
 
-echo "Downloading Haiku source from Google Drive..."
-curl -L -c cookies.txt "https://drive.google.com/uc?export=download&id=${FILE_ID}" -o temp.html
-CONFIRM=$(grep -o 'confirm=[^&]*' temp.html | head -n 1 | cut -d= -f2)
-curl -L -b cookies.txt "https://drive.google.com/uc?export=download&confirm=${CONFIRM}&id=${FILE_ID}" -o "${DEST}"
+# Download Haiku ZIP from Google Drive (large files supported)
+gdown https://drive.google.com/uc?id=1jN423vDbJzPb0G9rmgGDi_HhbE73r_iI -O haiku.zip
 
-# Unzip source
-unzip -q "${DEST}"
+# Unzip
+unzip -q haiku.zip
 cd haiku
 
-# Install build dependencies (Ubuntu runner)
+# Install dependencies
 sudo apt-get update
 sudo apt-get install -y build-essential git automake autoconf \
     flex bison gawk texinfo gcc-multilib g++-multilib zlib1g-dev
 
-# Configure and build
+# Build Haiku
 ./configure --build-cross-tools x86_64
 jam -q haiku-image
